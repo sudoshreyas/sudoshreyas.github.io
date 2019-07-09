a0c45cc9f74a9e8a05ef59f633f5d6bb59a9a75d
Shubham-Kumar-2000 <shukhu10@gmail.com>
2019-07-09 13:24:50 +0530

    Updates

---
+++ precache-manifest.180c6bdb4738662addb1e4be57e226e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f175e794ad19528cfa9f3efe181451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598210c8aa09fa9e9b1b",
+    "url": "/static/css/main.71fe47d3.chunk.css"
+  },
+  {
+    "revision": "501a0f83a0d8d60f8f25",
+    "url": "/static/js/2.0ad4b4ee.chunk.js"
+  },
+  {
+    "revision": "598210c8aa09fa9e9b1b",
+    "url": "/static/js/main.6ca03d03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a09d0f7c1ffb6d709113a261b0c8f11",
+    "url": "/static/media/ui.5a09d0f7.png"
+  }
+]);
